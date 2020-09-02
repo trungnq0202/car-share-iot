7897ddffe28d08832de8117ab9b5ac1c5079cc77
Trung Ngo Quang <S3742774@rmit.edu.vn>
2020-09-03 00:00:00 +0700

    add CRUD for admin

--- masterpi/app/cars/models.py
+++ masterpi/app/cars/models.py
@@ -1,5 +1,6 @@
 from app import db
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
 class Car(db.Model):
     __tablename__ = 'Car'
@@ -9,14 +10,23 @@ class Car(db.Model):
     body_type = db.Column(db.String(100))
     seats = db.Column(db.Integer)
     cost_per_hour = db.Column(db.Integer, nullable=False)
+    available = db.Column(db.Boolean, nullable=False)
+    reports = relationship("CarReport")
 
     def __init__(self, make=None, color=None, body_type=None, 
-                 seats=None, cost_per_hour=0):
+                 seats=None, cost_per_hour=0, available=True):
         self.make = make
         self.color = color
         self.body_type = body_type
         self.seats = seats
         self.cost_per_hour = cost_per_hour
+        self.available = available
+
+    @property
+    def availability(self):
+        if self.reports and not self.reports[-1].fixed:
+            return "fixing"
+        return "yes"
 
     def __repr__(self):
         return '<Car %r>' % (self.id)
@@ -44,8 +54,10 @@ class CarReport(db.Model):
     creation_time = db.Column(db.DateTime(timezone=True), server_default=func.now())
     update_time = db.Column(db.DateTime(timezone=True), onupdate=func.now())
 
-    def __init__(self, car_id, fixer_id):
+    def __init__(self, car_id):
         self.car_id = car_id
+
+    def setFixer(self, fixer_id):
         self.fixer_id = fixer_id
 
     def setFixed(self, fixed=True):
