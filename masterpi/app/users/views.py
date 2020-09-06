5a54e42d8749210ab8e053d3422c1c107eb29eeb
Trung Ngo Quang <S3742774@rmit.edu.vn>
2020-09-07 00:00:00 +0700

    redirect users to appropriate page upon login

--- masterpi/app/users/views.py
+++ masterpi/app/users/views.py
@@ -11,20 +11,6 @@ from flask_login import (
 
 from app import db, login_manager, client
 from app.users.forms import RegisterForm, LoginForm, UserForm, UserEditForm
-import os
-import pickle
-import json
-import numpy as np
-from json import JSONEncoder
-from multiprocessing import Process
-from ..facial_recognition.encode_faces import encode
-from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
-from werkzeug.security import check_password_hash, generate_password_hash
-from sqlalchemy import or_
-from werkzeug.utils import secure_filename
-
-from app import db
-from app.users.forms import RegisterForm, LoginForm, PhotosForm
 from app.users.models import User
 from app.cars.models import Car, CarReport
 from app.cars.forms import CarForm
@@ -33,23 +19,32 @@ from app.bookings.models import Booking
 
 mod = Blueprint('users', __name__, url_prefix='/users')
 api_mod = Blueprint('users_api', __name__, url_prefix='/api/users')
-UPLOAD_FOLDER_URL = 'app/facial_recognition/dataset'
 
-class NumpyArrayEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        else:
-            return super(NumpyArrayEncoder, self).default(obj)
+@mod.route('/login/redirect')
+@login_required
+def login_redirect():
+    if current_user.isEngineer():
+        return redirect(url_for('users.engineer'))
+    if current_user.isManager() or current_user.isAdmin():
+        return redirect(url_for('users.dashboard'))
+    return redirect(url_for('users.home'))
 
-@mod.route('/me/')
+@mod.route('/')
 @login_required
 def home():
-    return render_template("users/profile.html", user=current_user)
+    bookings = Booking.query.filter_by(user_id=current_user.id).all()
+    return render_template("users/home.html", user=current_user, bookings=bookings)
+    
+@mod.route('/dashboard/')
+@login_required
+def dashboard():
+    return render_template("users/dashboard.html")
+    
+@mod.route('/engineer/')
+@login_required
+def engineer():
+    return render_template("users/engineer.html")
+    
 
 @mod.route('/logout/')
 @login_required
@@ -84,8 +79,7 @@ def login():
             # the session can't be modified as it's signed, 
             # it's a safe place to store the user id
             login_user(user)
-            flash('Welcome %s' % user.username)
-            return redirect(url_for('users.home'))
+            return redirect(url_for('users.login_redirect'))
         flash('Wrong username or password', 'error-message')
     return render_template("users/login.html", form=form)
 
@@ -137,7 +131,7 @@ def google_callback():
         db.session.add(user)
         db.session.commit()
     login_user(user)
-    return redirect(url_for('users.home'))
+    return redirect(url_for('users.login_redirect'))
 
 @mod.route('/register/', methods=['GET', 'POST'])
 def register():
@@ -163,11 +157,8 @@ def register():
 
             # Log the user in, as he now has an id
             login_user(user)
-
-            # flash will display a message to the user
-            flash('Thanks for registering')
             # redirect user to the 'home' method of the user module.
-            return redirect(url_for('users.home'))
+            return redirect(url_for('users.login_redirect'))
         flash('Email address or username is taken.')
     return render_template("users/register.html", form=form)
     
@@ -311,31 +302,3 @@ def api_login():
 
 def api_logout():
     pass
-
-@api_mod.route('/face_encodings/', methods=['GET'])
-def face_encodings():
-    encodings = pickle.loads(open('app/facial_recognition/output/encodings.pickle', 'rb').read())
-    encodings_json = json.dumps(encodings, cls=NumpyArrayEncoder)
-    return encodings_json
-
-@mod.route('/photos-upload/', methods=['GET', 'POST'])
-@login_required
-def photos_upload():
-    """
-    Upload photos
-    """
-    form = PhotosForm()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            user = User.query.filter_by(id=session['user_id']).first()
-            username = user.username
-            directory = os.path.join(UPLOAD_FOLDER_URL, username)
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            for f in request.files.getlist('images'):
-                filename = secure_filename(f.filename)
-                f.save(os.path.join(directory, filename))
-            thread = Process(target=encode)
-            thread.run()
-            return render_template("users/photos-upload.html", form=form, uploaded=True)
-    return render_template("users/photos-upload.html", form=form, uploaded=False)
