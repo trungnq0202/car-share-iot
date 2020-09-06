06ab00b29be88e71a2a72e6bd97041a9d9d5d7c8
Trung Ngo Quang <S3742774@rmit.edu.vn>
2020-09-07 00:00:00 +0700

    add google login feature

--- masterpi/app/users/forms.py
+++ masterpi/app/users/forms.py
@@ -1,8 +1,7 @@
-from flask_wtf import Form, RecaptchaField, FlaskForm
-from wtforms import TextField, PasswordField, BooleanField, MultipleFileField, SubmitField
+from flask_wtf import Form, RecaptchaField
+from wtforms import TextField, PasswordField, BooleanField, SelectField
 from wtforms.validators import Required, EqualTo, Email
-from flask_wtf.file import FileAllowed, FileRequired
-from wtforms import FileField
+from app.users import constants as USER
 
 class LoginForm(Form):
     username = TextField('Username', [Required()])
@@ -19,9 +18,23 @@ class RegisterForm(Form):
         ])
     accept_tos = BooleanField('I accept the TOS', [Required()])
 
-class PhotosForm(FlaskForm):
-    images = FileField('Images', render_kw={'multiple':True},
-    validators=[
-        FileRequired(),
-        FileAllowed(['jpg', 'png'], 'jpg, png files only!')
-    ])
+class UserForm(Form):
+    username = TextField('Username', [Required()])
+    email = TextField('Email address', [Required(), Email()])
+    password = PasswordField('Password', [Required()])
+    role = SelectField('Role', choices=[(0, USER.ROLE[0]), 
+                                       (1, USER.ROLE[1]), 
+                                       (2, USER.ROLE[2]),
+                                       (3, USER.ROLE[3])])
+    first_name = TextField('First Name')
+    last_name = TextField('Last Name')
+
+class UserEditForm(Form):
+    username = TextField('Username', [Required()])
+    email = TextField('Email address', [Required(), Email()])
+    role = SelectField('Role', choices=[(0, USER.ROLE[0]), 
+                                       (1, USER.ROLE[1]), 
+                                       (2, USER.ROLE[2]),
+                                       (3, USER.ROLE[3])])
+    first_name = TextField('First Name')
+    last_name = TextField('Last Name')
